cf3c80b1fd1b25759cdf52f13e117a0353f420a0
Evfimiy Smirnov <evfim1234@gmail.com>
2022-11-06 20:35:36 +0300

    #2 added function that gets input

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def get_name():
+    name = input()
+    return name
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(get_name())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
